45b60c95ec2722c1d865776541007fcbaaa6dd86
Umendra A Bisen <umendrabisen2005@gmail.com>
2025-06-23 18:47:40 +0530

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,6 +8,7 @@ const HeroSection = () => {
       <div className="container">
         <div className="banner">
           <div className="largeBox">
+                  <h1 className="title">Good Food, Good Mood</h1>
           <h1 className="title">Delicious</h1>
           </div>
           <div className="combined_boxes">
